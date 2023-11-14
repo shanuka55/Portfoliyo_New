f90a7b993b84ef110c6ae7e969495ac00921e46d
shanuka55 <shanukalakshan555@gmail.com>
2023-11-14 16:13:04 +0530

    Project start

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='header'>
+      <h1>Shanuka</h1>
+      <ul>
+        <li>
+          <link>Home</link>
+        </li>
+      </ul>
+
+    </div>
+  )
+}
