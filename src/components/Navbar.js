6474855a11a1c3d1d943c4c0b04b2d2266c93066
shanuka55 <shanukalakshan555@gmail.com>
2023-11-14 18:02:28 +0530

    Navigation is done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,26 @@ const Navbar = () => {
       <Link to="/">
         <h1>Shanuka</h1>
       </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/skill">Skill</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   )
 }
