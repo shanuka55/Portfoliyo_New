0172e2a6683ab124688be1a699efde811aece155
shanuka55 <shanukalakshan555@gmail.com>
2023-11-15 17:04:22 +0530

    Home page updated

--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import "./HometextStyle.css"
+import { Link } from "react-router-dom";
 
 const HomeText = () => {
   return (
     <div className='Home-Text'>
+      <div className='container'>
         <h1 className='home-hi'>Hey There, </h1>
         <h1 className='home-h1'>I’m Shanuka Lakshan</h1>
         <h3 className='home-h3'>Full stack Developer</h3>
         <br></br>
-        <button className='btn'>Get Start</button>
+        <div>
+          <Link to="/about" className='btn'>Get Start </Link>
+        </div>
+      </div>    
     </div>
   )
 }
