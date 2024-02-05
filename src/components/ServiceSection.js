8efd1a7cb3550cee8262b3c3f57cde3afa0418c5
shanuka55 <shanukalakshan555@gmail.com>
2024-02-05 17:05:22 +0530

    updated

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./ServiceSectionStyle.css"
 import { FaCode } from "react-icons/fa";
+import { colors } from '@mui/material';
 
 export const ServiceSection = () => {
   return (
     <div>
         <div className='serviceContainer'>
-            <div className='box' >
+            <div className='box'>
                 <div className='content'>
-                    <div className='icon'><FaCode size={25} /></div>
+                    <div className='Serviceicon'><FaCode size={25} /></div>
                     <div className='text'>
                         <h3>Designing</h3>
                         <p>Lorem</p>
@@ -18,7 +19,7 @@ export const ServiceSection = () => {
             </div>
             <div className='box' >
                 <div className='content'>
-                    <div className='icon'></div>
+                    <div className='Serviceicon'></div>
                     <div className='text'>
                         <h3>Development</h3>
                         <p>Lorem</p>
@@ -28,7 +29,7 @@ export const ServiceSection = () => {
             </div>
             <div className='box' >
                 <div className='content'>
-                    <div className='icon'></div>
+                    <div className='Serviceicon'></div>
                     <div className='text'>
                         <h3>SEO</h3>
                         <p>Lorem</p>
